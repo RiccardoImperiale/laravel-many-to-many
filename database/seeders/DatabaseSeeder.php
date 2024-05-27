71938a4026cad248d80779043a4e053f21a8e41e
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-27 12:28:47 +0200

    add many to many relationship to Technology and Project models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call(
             [
                 TypeSeeder::class,
-                ProjectSeeder::class
+                ProjectSeeder::class,
+                TechnologySeeder::class
             ]
         );
     }
